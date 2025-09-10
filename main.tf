29b26ff98f07e68c172b3d7421f9f9b9fcb163fb
balinas-ramon <balinas_ramon@bah.com>
2025-09-10 12:16:01 -0400

    Initial push of GH-ACTIONS-TERRDEv-01 project

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  profile = "rbal"      # Your AWS CLI profile name
+  region = "us-east-1"
+}
+
+#tfsec:ignore:aws-s3-enable-bucket-logging
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket-999"
+}
